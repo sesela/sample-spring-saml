cb4337afba25a96f2166d38b010a8aff02cf6caf
sesela <kudo.shinya01@gmail.com>
2019-11-17 21:29:09 +0900

    no message

--- src/main/java/net/sesela/sample/spring/saml/security/SecurityConfig.java
+++ src/main/java/net/sesela/sample/spring/saml/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package net.sesela.sample.spring.saml.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
